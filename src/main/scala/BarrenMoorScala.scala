d320f8174d83881ab54ca1f49a108a4d26214164
Daniel Smith <danielsmith.2796@googlemail.com>
2019-07-06 19:23:12 +0100

    paragraph two of introduction now prints

--- src/main/scala/BarrenMoorScala.scala
+++ src/main/scala/BarrenMoorScala.scala
@@ -1,5 +1,7 @@
 object Main extends App {
-
   import StoryText.introOne
+  import StoryText.introTwo
+
   println(introOne)
+  println(introTwo)
 }
--- src/main/scala/StoryText.scala
+++ src/main/scala/StoryText.scala
@@ -3,4 +3,8 @@ object StoryText {
     "you begin to make out details and shapes. In front of you you see a vast, open, and almost entirely empty swamp. " +
     "With no memory of who you are, how you got here, or why you would be here in the first place, you set out to " +
     "find help."
+
+  val introTwo = "Before setting out on your adventure you quickly search through your pockets to find some clues " +
+    "about how you ended up in this situation. However all you find is a highly decorated, but aged compass " +
+    "nestled in the pocket of your breeches. Could it be a good idea to follow this?"
 }
