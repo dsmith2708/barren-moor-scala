3161bcaf58a1836f81881505c501b4340dad7dc0
Daniel Smith <danielsmith.2796@googlemail.com>
2019-07-06 19:11:05 +0100

    first part of story introduction now displays

---
+++ src/main/scala/StoryText.scala
@@ -0,0 +1,6 @@
+object StoryText {
+  val introOne = "You open your eyes to see a mix of brown and green; your vision hazy at first and slowly clearing " +
+    "you begin to make out details and shapes. In front of you you see a vast, open, and almost entirely empty swamp. " +
+    "With no memory of who you are, how you got here, or why you would be here in the first place, you set out to " +
+    "find help."
+}
