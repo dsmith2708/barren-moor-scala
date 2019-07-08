cedb28ccb004a370de589d20f152ca91b3cab6cf
Daniel Smith <danielsmith.2796@googlemail.com>
2019-07-08 20:38:13 +0100

    Added game states and main game loop

--- src/main/scala/BarrenMoorScala.scala
+++ src/main/scala/BarrenMoorScala.scala
@@ -1,15 +1,40 @@
 package barrrenMoor
 
 object Main extends App {
+
+  // Necessary Imports
   import barrenMoor.StoryText.introOne
   import barrenMoor.StoryText.introTwo
   import barrenMoor.StoryText.toolTipOne
   import barrenMoor.StoryText.toolTipTwo
 
+  // Game State Enumeration
+  object GameStates extends Enumeration {
+    val starting, running, complete = Value
+  }
+
+  // Variable for current game state
+  var gameState = GameStates.starting
+
+  // Logic for game start
+  println("Game starting")
   println(introOne)
   println(introTwo)
   println(toolTipOne)
   println(toolTipTwo)
+  gameState = GameStates.running
+
+  // Main Game Loop
+  while (gameState == GameStates.running) {
+    println("Game Running")
+    gameState = GameStates.complete
+  }
+  // Game Shutdown code
+  println("Game ending")
+
+
+
+
 
 
 }
